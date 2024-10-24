66bcc2ad391eb881da6ee6bbf771c13b08f7265e
unknown <prathameshshriramwar100@gmail.com>
2024-10-24 17:26:20 +0530

    changes in Db Connection

--- database/db.js
+++ database/db.js
@@ -8,17 +8,26 @@ class DB {
   static #isConnected = false;
 
   static async connect() {
+    // if (!this.#pool) {
+    //   this.#pool = new Pool({
+    //     user: PGUSER,
+    //     password: PGPASSWORD,
+    //     host: PGHOST,
+    //     port: PGPORT,
+    //     database: PGDATABASE,
+    //     ssl: {
+    //       rejectUnauthorized: false,
+    //     },
+    //   });
+
     if (!this.#pool) {
       this.#pool = new Pool({
-        user: PGUSER,
-        password: PGPASSWORD,
-        host: PGHOST,
-        port: PGPORT,
-        database: PGDATABASE,
-        ssl: {
-          rejectUnauthorized: false,
-        },
+        connectionString: `${DATABASE_URL}/${DATABASE_NAME}`,
+        // ssl: NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
+        ssl: false,
       });
+
+      
 
       this.#pool.on("error", (err) => {
         console.error("Unexpected error on idle client", err);
