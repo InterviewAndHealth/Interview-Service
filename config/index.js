6e36fb30ab0fe139c1e4d85758490c9a08334069
GopalSaraf <sarafgopal37@gmail.com>
2024-11-25 19:29:54 +0530

    Added workflow and updated env names

--- config/index.js
+++ config/index.js
@@ -6,15 +6,31 @@ dotEnv.config();
 module.exports = {
   PORT: process.env.PORT || 8000,
   APP_SECRET: process.env.APP_SECRET,
-  DATABASE_URL: process.env.DATABASE_URL,
-  DATABASE_NAME: process.env.DATABASE_NAME,
-  RABBITMQ_URL: process.env.RABBITMQ_URL,
+
+  POSTGRES_USERNAME: process.env.POSTGRES_USERNAME,
+  POSTGRES_PASSWORD: process.env.POSTGRES_PASSWORD,
+  POSTGRES_HOST: process.env.POSTGRES_HOST,
+  POSTGRES_PORT: process.env.POSTGRES_PORT,
+  DATABASE_URL:
+    process.env.DATABASE_URL ||
+    `postgresql://${process.env.POSTGRES_USERNAME}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}`,
+  DATABASE_NAME: process.env.DATABASE_NAME || process.env.INTERVIEW_SERVICE_DB,
+
+  RABBITMQ_USERNAME: process.env.RABBITMQ_USERNAME,
+  RABBITMQ_PASSWORD: process.env.RABBITMQ_PASSWORD,
+  RABBITMQ_HOST: process.env.RABBITMQ_HOST,
+  RABBITMQ_PORT: process.env.RABBITMQ_PORT,
+  RABBITMQ_URL:
+    process.env.RABBITMQ_URL ||
+    `amqp://${process.env.RABBITMQ_USERNAME}:${process.env.RABBITMQ_PASSWORD}@${process.env.RABBITMQ_HOST}:${process.env.RABBITMQ_PORT}`,
+
   EXCHANGE_NAME: process.env.EXCHANGE_NAME,
-  SERVICE_NAME: process.env.SERVICE_NAME,
-  SERVICE_QUEUE: process.env.SERVICE_QUEUE,
-  RPC_QUEUE: process.env.RPC_QUEUE,
-  USERS_QUEUE: process.env.USERS_QUEUE,
-  USERS_RPC: process.env.USERS_RPC,
+  SERVICE_NAME: process.env.SERVICE_NAME || "INTERVIEW_SERVICE",
+  SERVICE_QUEUE: process.env.SERVICE_QUEUE || process.env.INTERVIEW_QUEUE,
+  RPC_QUEUE: process.env.RPC_QUEUE || process.env.INTERVIEW_RPC,
+
+  USERS_QUEUE: process.env.USER_QUEUE,
+  USERS_RPC: process.env.USER_RPC,
   PAYMENT_QUEUE: process.env.PAYMENT_QUEUE,
   PAYMENT_RPC: process.env.PAYMENT_RPC,
   EVENT_TYPES,
